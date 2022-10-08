689b9d00aca3eba7228a399ecc9cec49334d3d7b
Winifred <winifredasante15@gmail.com>
2022-10-08 01:48:40 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Users  from "./components/Users";
 import AddUserForm from "./components/AddUserForm";
